8674ee1bdf201f098b4e36a59d834786b2174e68
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2025-12-24 22:02:51 +0200

    Started frontend logic for authentication

--- studentHelper/app/_layout.tsx
+++ studentHelper/app/_layout.tsx
@@ -1,5 +1,32 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+import { SessionProvider, useSession } from '../ctx';
+import { SplashScreenController } from './splash';
+
+export default function Root() {
+ 
+  return (
+    <SessionProvider>
+      <SplashScreenController />
+      <RootNavigator />
+    </SessionProvider>
+  );
 }
+
+
+function RootNavigator() {
+  const { session } = useSession();
+
+  return (
+    <Stack>
+      <Stack.Protected guard={!!session}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
+
